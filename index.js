4c6b99494e9da389e212e2b481a81ce5637ac7ab
kamusarihanshi <ahmedizaka9@gmail.com>
2024-03-22 13:30:47 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 =31;
+let num2 = 2;
+const multiply=num1*num2;
+ 
+let random=2;
+random>=2;
 
-// Write your code here
+
+
+let num3=6;
+let num4=16;
+let mod=num4 % num3;
+
+let max=Math.max(5,10,15,20);
+console.log(max);
+
+
+
